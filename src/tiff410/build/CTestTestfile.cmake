561f1f16a8b87e992298c163021d24fc9cae7062
GasPanic <gaspanic987@gmail.com>
2020-09-29 19:07:16 -0300

    fixing conflicts

--- src/tiff410/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andreas/Moln/GitHub/rcell2/src/tiff410/build
-# Build directory: /Users/andreas/Moln/GitHub/rcell2/src/tiff410/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
