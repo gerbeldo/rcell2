e9354498cd7a3eeeee75e6ddf4ba0c47c01cbf50
GasPanic <gaspanic987@gmail.com>
2020-09-29 18:57:47 -0300

    cleaning up from previous commit

--- src/tiff410/contrib/addtiffo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andreas/Moln/GitHub/rcell2/src/tiff410/contrib/addtiffo
-# Build directory: /Users/andreas/Moln/GitHub/rcell2/src/tiff410/contrib/addtiffo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/tiff410/contrib/mfs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andreas/Moln/GitHub/rcell2/src/tiff410/contrib/mfs
-# Build directory: /Users/andreas/Moln/GitHub/rcell2/src/tiff410/contrib/mfs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
